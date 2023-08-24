314962b4584344cbbf674bd8c7b6f34853ee8c36
LinusTorvaldCoolMan <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-08-24 04:20:19 +0300

    Update charAt.cpp

--- src/charAt.cpp
+++ src/charAt.cpp
@@ -1,6 +1,6 @@
 #include <DiggerString.h>
 namespace DiggerString {
-    static char charAt(const std::string& str, int index) {
+     char DiggerString::charAt(const std::string& str, int index) {
         if (index >= 0 && index < static_cast<int>(str.length())) {
             return str[index];
         }
