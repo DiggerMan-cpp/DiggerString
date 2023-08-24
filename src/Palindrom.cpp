562d1dda0626136e5ac441987e3bedc785bea1e9
LinusTorvaldCoolMan <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-08-24 04:22:04 +0300

    Delete Palindrom.cpp

--- src/Palindrom.cpp
+++
@@ -1,7 +0,0 @@
-#include <DiggerString.h>
-namespace DiggerString {
-    bool DiggerString::isPalindrome(const std::string& str) {
-        std::string reversed = reverse(str);
-        return str == reversed;
-    }
-}
