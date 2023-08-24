978e4ab144445f7ebdbbb092d36e0ed95cdc5b8c
LinusTorvaldCoolMan <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-08-24 04:30:42 +0300

    Delete substring.cpp

--- src/substring.cpp
+++
@@ -1,6 +0,0 @@
-#include <DiggerString.h>
-namespace DiggerString {
-	std::string DiggerString::substring(const std::string& str, int startIndex, int length) {
-		return str.substr(startIndex, length);
-	}
-}
