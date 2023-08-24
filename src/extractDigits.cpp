c012c81427d3bc02695192af4d6bed69abfb1b2f
LinusTorvaldCoolMan <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-08-24 04:14:17 +0300

    Update extractDigits.cpp

--- src/extractDigits.cpp
+++ src/extractDigits.cpp
@@ -1,6 +1,6 @@
 #include <DiggerString.h>
 namespace DiggerString {
-    static std::string DiggerString::extractDigits(const std::string& str) {
+     std::string DiggerString::extractDigits(const std::string& str) {
         std::string result;
         for (char ch : str) {
             if (std::isdigit(ch)) {
